4bb023ce72439565c832d7e2b65061d418e279b1
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-06 16:38:56 +0100

    Initial Commit

---
+++ SnapChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapChat
+//
+//  Created by Emil Vaklinov on 06/05/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
